26f9250c42cd0234a152b47bec33133db1121963
ragini412 <raginigandharv05@gmail.com>
2024-11-17 13:35:14 -0500

    images
    image changes

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+var icon= document.getElementById("desc-icon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src="./images/sun.png";
+  }else{
+    icon.src="./images/moon.png";
+  }
+}
+
+var icon= document.getElementById("ham-icon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src="./images/sun.png";
+  }else{
+    icon.src="./images/moon.png";
+  }
+}
